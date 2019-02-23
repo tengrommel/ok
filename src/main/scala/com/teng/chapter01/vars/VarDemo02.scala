eff215052ae5a2cbeb5f35cf504f126ce37bf364
tengrommel <wwwsousoucom@126.com>
2019-02-24 01:43:18 +0800

    class 的错误

--- src/main/scala/com/teng/chapter01/vars/VarDemo02.scala
+++ src/main/scala/com/teng/chapter01/vars/VarDemo02.scala
@@ -6,5 +6,12 @@ object VarDemo02 {
     age = 30
     val num2 = 30
 //    num2 = 40 报错
+    val dog = new Dog
+//    dog = new Dog
   }
+}
+
+class Dog {
+  var age: Int = 0
+  var name: String = ""
 }
